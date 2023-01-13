81b3d4fe402d3abbfb8574834d5bf7b904005f7e
Ari-Jackson <112004676+Ari-Jackson@users.noreply.github.com>
2023-01-13 16:51:25 -0500

    Adds search result modal with styling for album results

--- main.js
+++ main.js
@@ -1,18 +1,109 @@
 /* <------------------------- Selectors ------------------------->*/
+//main page
 const mainArticle = document.querySelector(".js-main-article");
 const searchBtn = document.querySelector(".js-submit-button");
 const textInput = document.querySelector(".js-input-field");
 const radioOptions = document.querySelectorAll(".js-radio");
 const form = document.querySelector(".js-form");
 const mainHeading = document.querySelector(".js-main-heading");
+//modal
 const dialog = document.querySelector(".js-modal");
+const dialogMain = document.querySelector(".js-modal-section");
+const closeModal = document.querySelector(".js-x-button");
 
 /* <------------------------- Event Listeners------------------------->*/
+//on load
 document.addEventListener("DOMContentLoaded", renderTenNewReleases);
-searchBtn.addEventListener("click", renderSearchResuls);
+//on search
+searchBtn.addEventListener("click", renderSearchResults);
+
+//open modal on line 167
+
+//close modal
+closeModal.addEventListener("click", () => dialog.close());
+dialog.addEventListener("close", () => modalPlay.removeAttribute("id"));
+
 /* <------------------------- Functions ------------------------->*/
+//Dialog Functions
+async function showSongOrAlbumModal(e) {
+  dialogMain.innerHTML = "";
+
+  const result = await fetchSingleResult(e);
+
+  renderModalImg(result);
+  renderResultModal(result);
+
+  dialog.showModal();
+}
+
+function renderModalImg(result) {
+  
+  const img = document.createElement("img");
+  img.setAttribute(
+    "src",
+    result.type == "track" ? result.album.images[1].url : result.images[1].url
+  );
+  img.setAttribute("class", "album-cover");
+
+  dialogMain.append(img);
+}
+
+function renderResultModal(result) {
+  console.log(result);
+  // heading
+
+  const name = document.createElement("h4");
+  name.textContent = `${result.name}`;
+
+  //main content
+  const artist = document.createElement("p");
+  artist.innerHTML = `<strong>Artists: </strong> ${result.artists
+    .map((artist) => artist.name)
+    .join(", ")}`;
+
+  //just for albums
+  const albumType = document.createElement("h5");
+  albumType.textContent = result.album_type == "single" ? "Single" : "Album";
+
+  const released = document.createElement("p");
+  released.innerHTML = `<strong>Realease Date: </strong> ${new Date(
+    result.release_date
+  ).toLocaleDateString("en-us", {
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+  })}`;
+
+  const recordingLabel = document.createElement("p");
+  recordingLabel.innerHTML = `<strong>Record Label: </strong> ${result.label}`;
+
+  const copyright = document.createElement("p");
+  copyright.textContent = `${result.copyrights[0].text}`;
+
+  //append
+  const div = document.createElement("div");
+  div.append(name, albumType, artist, released, recordingLabel, copyright);
+
+  dialogMain.append(div);
+}
+
+async function fetchSingleResult(e) {
+  const type = e.target.id.split(" ");
+
+  const requestOptions = await createRequestOptions();
+
+  let resultRaw = await fetch(
+    `https://api.spotify.com/v1/${type[0]}s/${type[1]}`,
+    requestOptions
+  );
+
+  let result = await resultRaw.json();
+
+  return result;
+}
+
 //Seach button functions
-async function renderSearchResuls(e) {
+async function renderSearchResults(e) {
   e.preventDefault();
 
   const searchResults = await returnTenSearchResults();
@@ -45,8 +136,6 @@ async function returnTenSearchResults() {
 //On Load Functions
 async function renderTenNewReleases() {
   let topTen = await fetchTopTen();
-  console.log(topTen);
-
   topTen.albums.items.forEach(renderResult);
 }
 
@@ -68,9 +157,10 @@ function renderResult(result) {
 
   const div = document.createElement("div");
   div.setAttribute("class", "card");
-  div.setAttribute("id", result.id);
+  div.setAttribute("id", result.type + " " + result.id);
+
   div.append(img, h4, p);
-  //   div.addEventListener("click", showSongOrAlbumModal)
+  div.addEventListener("click", showSongOrAlbumModal);
 
   mainArticle.append(div);
 }
